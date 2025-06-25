d8ba2ed0d3094d8efa027248a435845b33d7a227
Zaher Madi <zahermadi@yahoo.fr>
2025-06-25 11:42:30 +0200

    test

--- server/server.py
+++ server/server.py
@@ -6,15 +6,15 @@ from fastapi.responses import HTMLResponse, JSONResponse
 # from fastapi import Form
 
 app = FastAPI()
-# origins = ["*"]
+origins = ["*"]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # # #Create a connection to the database
 # # conn = mysql.connector.connect(
